62980a71ac1591b7219eb0193dbd7bf15c962e71
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-07-15 18:22:56 +0500

    Enhance logger with timestamp

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const { add, divide, multiply } = require('./utils/math');
+const { log } = require('./utils/logger');
+log("---Executing math functions---");
 console.log('Sum:', add(2, 3));
 console.log('Divide:', divide(2, 3));
 console.log('Multiply:', multiply(2, 3));
