7efeba9a1df62a459218d8e515b26fb2571ba185
Muhammad Ali Mustafa <ali.mustafa@qbatch.com>
2025-07-15 18:09:18 +0500

    Add and use multiply function
    Use multiply and divide function in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { add, divide, multiply } = require('./utils/math');
+const { add, divide, multiply, multiply } = require('./utils/math');
 const { log } = require('./utils/logger');
 log("---Executing math functions---");
 console.log('Sum:', add(2, 3));
